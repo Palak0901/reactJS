068a4714517be841b81f6f7c2539b7c490368611
Palak0901 <palakgada.vhits@gmail.com>
2022-09-05 19:27:36 +0530

    Added persist store for permanent storing

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import "./App.css";
 import Route from "./navigation/Route";
 import store from "./services/redux/store";
 import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 
 function App() {
+  let persistor = persistStore(store);
   return (
     <Provider store={store}>
-      <Route />
+      <PersistGate persistor={persistor}>
+        <Route />
+      </PersistGate>
     </Provider>
   );
 }
