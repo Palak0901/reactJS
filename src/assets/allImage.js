2cbb42088b0ef052674fc6be51c460de2f09554e
Palak0901 <palakgada.vhits@gmail.com>
2022-09-06 12:06:29 +0530

    Added images and its import

---
+++ src/assets/allImage.js
@@ -0,0 +1,10 @@
+import svgIcon from "./svgIcon";
+
+export default {
+  checkImage: require("./image/check.png"),
+  emptyImage: require("./image/empty.png"),
+  hideImage: require("./image/hide.png"),
+  image: require("./image/image.png"),
+  viewImage: require("./image/view.png"),
+  svgIcon: svgIcon,
+};
