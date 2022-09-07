55ab868937d1a8808aea36a02033743bff7bb0f1
Palak0901 <palakgada.vhits@gmail.com>
2022-09-07 10:53:30 +0530

    Added register reducer to root reducer

--- src/services/redux/rootReducer.js
+++ src/services/redux/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
+import registerReducer from "./registerUser/reducer";
 import userReducer from "./user/reducer";
 
 const RootReducer = combineReducers({
   userReducer: userReducer,
+  registerReducer: registerReducer,
 });
 
 export default RootReducer;
