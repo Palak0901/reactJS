dae840b6e077636803411a6b184eb555f12fd605
Palak0901 <palakgada.vhits@gmail.com>
2022-09-07 10:48:53 +0530

    Made components import flie

---
+++ src/utils/components.js
@@ -0,0 +1,17 @@
+import ButtonComponent from "../components/buttonComponent";
+import DivComponent from "../components/divComponent";
+import ImageComponent from "../components/imageComponent";
+import InputComponent from "../components/inputComponent";
+import LabelComponent from "../components/labelComponent";
+import CustomDrawer from "../navigation/drawer/CustomDrawer";
+import Navbar from "../navigation/topNav/CustomTop";
+
+export default {
+  labelComponent: LabelComponent,
+  buttonComponent: ButtonComponent,
+  customTop: Navbar,
+  customDrawer: CustomDrawer,
+  inputComponent: InputComponent,
+  divComponent: DivComponent,
+  imageComponent: ImageComponent,
+};
