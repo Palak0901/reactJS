fb182a95e026bcf982bde6e6c1f520acc53c4b4f
Palak0901 <palakgada.vhits@gmail.com>
2022-09-07 10:51:37 +0530

    Added utils to validation component

--- src/components/ValidationComponent.js
+++ src/components/ValidationComponent.js
@@ -1,21 +1,17 @@
-import {
-  checkEmail,
-  checkNumber,
-  checkPassword,
-  checkString,
-} from "../utils/StringValidation";
+import Utils from "../utils";
 
 const LoginValidation = (email, password) => {
   const validationObj = { emailError: null, passwordError: null };
   if (!email) {
-    validationObj.emailError = "Please enter Email";
-  } else if (!checkEmail(email)) {
-    validationObj.emailError = "Please enter valid Email";
+    validationObj.emailError = Utils.validationMessage.validationEmail;
+  } else if (!Utils.validationImport.checkEmail(email)) {
+    validationObj.emailError = Utils.validationMessage.validationValidEmail;
   }
   if (!password) {
-    validationObj.passwordError = "Please enter Password";
-  } else if (!checkPassword(password)) {
-    validationObj.passwordError = "Please enter valid Password";
+    validationObj.passwordError = Utils.validationMessage.validationPassword;
+  } else if (!Utils.validationImport.checkPassword(password)) {
+    validationObj.passwordError =
+      Utils.validationMessage.validationValidPassword;
   }
   return validationObj;
 };
@@ -37,32 +33,32 @@ const RegisterValidation = (
   };
   if (!firstName) {
     validationObj.firstNameError = "Please enter First Name";
-  } else if (!checkString(firstName)) {
+  } else if (!Utils.validationImport.checkString(firstName)) {
     validationObj.firstNameError = "Please enter valid First Name";
   }
   if (!lastName) {
     validationObj.lastNameError = "Please enter Last Name";
-  } else if (!checkString(lastName)) {
+  } else if (!Utils.validationImport.checkString(lastName)) {
     validationObj.lastNameError = "Please enter valid Last Name";
   }
   if (!mobileNumber) {
     validationObj.mobileNumberError = "Please enter Mobile Number";
-  } else if (!checkNumber(mobileNumber)) {
+  } else if (!Utils.validationImport.checkNumber(mobileNumber)) {
     validationObj.mobileNumberError = "Please enter valid Mobile Number";
   }
   if (!email) {
     validationObj.emailError = "Please enter Email";
-  } else if (!checkEmail(email)) {
+  } else if (!Utils.validationImport.checkEmail(email)) {
     validationObj.emailError = "Please enter valid Email";
   }
   if (!password) {
     validationObj.passwordError = "Please enter Password";
-  } else if (!checkPassword(password)) {
+  } else if (!Utils.validationImport.checkPassword(password)) {
     validationObj.passwordError = "Please enter valid Password";
   }
   if (!confirmPassword) {
     validationObj.confirmPasswordError = "Please enter Confirm Password";
-  } else if (!checkPassword(confirmPassword)) {
+  } else if (!Utils.validationImport.checkPassword(confirmPassword)) {
     validationObj.confirmPasswordError = "Please enter valid Confirm Password";
   } else if (password != confirmPassword) {
     validationObj.confirmPasswordError =
@@ -71,4 +67,4 @@ const RegisterValidation = (
   return validationObj;
 };
 
-export default { LoginValidation, RegisterValidation };
+export { LoginValidation, RegisterValidation };
