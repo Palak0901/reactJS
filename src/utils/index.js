b8fd52e02ce230aaacd2f74cb083d6ab77333a0d
Palak0901 <palakgada.vhits@gmail.com>
2022-09-07 10:49:16 +0530

    Made utils index file for imports

---
+++ src/utils/index.js
@@ -0,0 +1,21 @@
+import components from "./components";
+import navigationImport from "./navigationImport";
+import screenName from "./screenName";
+import validation from "./validation";
+import validationMessage from "./validationMessage";
+import allImage from "../assets/allImage";
+import services from "./services";
+import validationImport from "./validationImport";
+import reduxImport from "./reduxImport";
+
+export default {
+  components: components,
+  navigationImport: navigationImport,
+  screenName: screenName,
+  validation: validation,
+  validationMessage: validationMessage,
+  allImage: allImage,
+  services: services,
+  validationImport: validationImport,
+  reduxImport: reduxImport,
+};
